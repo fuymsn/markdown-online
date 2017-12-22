eadc95945540da78b8e4cd827867086c7b017f68
Young <fuymsn@hotmail.com>
2017-12-22 16:12:20 +0700

    update UI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <textarea v-model="textareaStr"></textarea>
+    <h1>Markdon Online</h1>
+    <textarea class="textarea" v-model="textareaStr"></textarea>
+    <button class="button" @click="renderMd">生成Markdown</button>
     <div v-html="text"></div>
-    <button @click="renderMd">生成Markdown</button>
   </div>
 </template>
 
@@ -37,12 +38,46 @@ export default {
 </script>
 
 <style>
+*{
+  user-select: none;
+}
+html{
+  -webkit-tap-highlight-color: transparent;
+  -webkit-text-size-adjust: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.textarea{
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 5px;
+  height: 100px;
+}
+
+.button{
+  display: block;
+  width: 100%;
+  border-radius: 5px;
+  border: 0px;
+  padding: 8px 0px;
+  background-color: #ee6868;
+  color: #fff;
+  font-size: 1.3rem;
+  outline: 0px;
+  transition: all 0.2s ease-out;
+  cursor: pointer;
+  margin: 10px 0px;
+}
+
+.button.active,
+.button:active,
+.button:hover{
+  background-color: #da5757;
+  box-shadow: 0 0 0 0.3rem rgba(220,53,69,.5);
 }
 </style>
